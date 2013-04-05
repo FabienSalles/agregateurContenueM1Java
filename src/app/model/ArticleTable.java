3a3054fe97d085205811b427adc5d4c2092db692
And0r <v.ducrocq@gmail.com>
2013-04-05 15:36:36 +0300

    Update ArticleTable.java

--- src/app/model/ArticleTable.java
+++ src/app/model/ArticleTable.java
@@ -1,12 +1,77 @@
 package app.model;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.LinkedHashSet;
 import java.util.Set;
 
+import app.core.Query;
+
 public class ArticleTable {
-	public static Set<Article> articles;
 	
-	public static Set<Article> searchArticlesByKeywords(String keywords)
+	public static Set<Article> getArticles() throws SQLException 
 	{
+		Set<Article> articles = new LinkedHashSet<Article>();
+		
+		Connection conn = Query.getInstance();
+		Statement stat = conn.createStatement();
+        ResultSet rs = stat.executeQuery("SELECT * FROM Articles;");
+        
+        while(rs.next()) {
+            Article a = new Article();
+        	
+        	a.setTitle(rs.getString("title"));
+        	a.setContent(rs.getString("content"));
+            a.setAuthor(rs.getString("author"));
+            a.setDate(rs.getString("date"));
+            
+            articles.add(a);
+        }
+        
+		return articles;
+	}
+	
+	public static Set<Article> searchArticlesByKeywords(String keywords) throws SQLException
+	{
+		if (keywords.length() < 1)
+			return null;
+		
+		String[] splittedKeywords =  keywords.split(" ");
+		
+		if (splittedKeywords.length < 1)
+			return null;
+		
+		String query = "SELECT * FROM Articles ";
+		
+		for (int i = 0; i < splittedKeywords.length; i++) {
+			if (i == 0) {
+				query += "WHERE title LIKE \"%" + splittedKeywords[i] + 
+						"%\" OR content LIKE \"%" + splittedKeywords[i] + "%\" ";
+			} else {
+				query += "OR title LIKE \"%" +  splittedKeywords[i] + 
+						"%\" OR content LIKE \"%" + splittedKeywords[i] + "%\" ";
+			}
+		}
+
+		Set<Article> articles = new LinkedHashSet<Article>();
+		
+		Connection conn = Query.getInstance();
+		Statement stat = conn.createStatement();
+        ResultSet rs = stat.executeQuery(query);
+        
+        while(rs.next()) {
+            Article a = new Article();
+        	
+        	a.setTitle(rs.getString("title"));
+        	a.setContent(rs.getString("content"));
+            a.setAuthor(rs.getString("author"));
+            a.setDate(rs.getString("date"));
+            
+            articles.add(a);
+        }
+        
 		return articles;
 	}
 }
